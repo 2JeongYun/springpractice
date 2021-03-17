4c5215448b68b8a24befb955ace9190eda8eb4cb
2JeongYun <leejeongyun828@gmail.com>
2021-03-17 18:12:57 +0900

    Implement posts list in index page

--- src/main/java/com/github/neukrang/springpractice/service/PostsService.java
+++ src/main/java/com/github/neukrang/springpractice/service/PostsService.java
@@ -2,13 +2,16 @@ package com.github.neukrang.springpractice.service;
 
 import com.github.neukrang.springpractice.domain.posts.Posts;
 import com.github.neukrang.springpractice.domain.posts.PostsRepository;
+import com.github.neukrang.springpractice.web.dto.PostsListResponseDto;
 import com.github.neukrang.springpractice.web.dto.PostsResponseDto;
 import com.github.neukrang.springpractice.web.dto.PostsSaveRequestDto;
 import com.github.neukrang.springpractice.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
-import javax.transaction.Transactional;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -36,5 +39,12 @@ public class PostsService {
                 .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
 
         return new PostsResponseDto(entity);
+    }
+
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc() {
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new)
+                .collect(Collectors.toList());
     }
 }
