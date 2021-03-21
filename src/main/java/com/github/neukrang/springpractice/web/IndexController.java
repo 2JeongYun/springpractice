348b3dfd1796105c6d770b1facc433327f3237f3
2JeongYun <leejeongyun828@gmail.com>
2021-03-21 19:25:25 +0900

    Add LoginUser annotation

--- src/main/java/com/github/neukrang/springpractice/web/IndexController.java
+++ src/main/java/com/github/neukrang/springpractice/web/IndexController.java
@@ -1,5 +1,6 @@
 package com.github.neukrang.springpractice.web;
 
+import com.github.neukrang.springpractice.config.auth.LoginUser;
 import com.github.neukrang.springpractice.config.auth.dto.SessionUser;
 import com.github.neukrang.springpractice.service.PostsService;
 import com.github.neukrang.springpractice.web.dto.PostsResponseDto;
@@ -19,9 +20,8 @@ public class IndexController {
     private final HttpSession httpSession;
 
     @GetMapping("/")
-    public String index(Model model) {
+    public String index(Model model, @LoginUser SessionUser user) {
         model.addAttribute("posts", postsService.findAllDesc());
-        SessionUser user = (SessionUser) httpSession.getAttribute("user");
         if (user != null) {
             model.addAttribute("loginUserName", user.getName());
         }
